4ceb0851aef67351284a8ddece35e9938f510dae
MATHYSLEOUF <mathys0@hotmail.fr>
2024-10-25 12:36:18 +0200

    3 commit

--- src/app/component/CookieZone.tsx
+++ src/app/component/CookieZone.tsx
@@ -1,21 +1,26 @@
 import Image from 'next/image';
-import BTCImg from '@/assets/img/Bitcoin.png'; 
+import BTCImg from '@/assets/img/Bitcoin.png';
+
+type OnCookieClick = () => void;
 
 interface Props {
   totalCookies: number;
   cookiesPerSecond?: number;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-  onCookieClick: Function;
-  cps: number
+  onCookieClick: OnCookieClick;
+  cps: number;
 }
 
-export const CookieZone = ({ totalCookies, onCookieClick, cps=0}: Props) => {
-
+export const CookieZone = ({ totalCookies, onCookieClick, cps = 0 }: Props) => {
   return (
     <div className="h-full w-full flex flex-col items-center justify-center">
       <p className='text-2xl font-bold'>BTC par secondes {cps}</p>
-        <p className='text-3xl font-bold'>Total BTC : {totalCookies.toFixed(0)}</p>
-      <Image onClick={() => onCookieClick()} src={BTCImg} alt="BTC Image" className="cursor-pointer h-52 hover:h-60 w-52 hover:w-64 ease-in-out duration-300" /> 
+      <p className='text-3xl font-bold'>Total BTC : {totalCookies.toFixed(0)}</p>
+      <Image 
+        onClick={onCookieClick} 
+        src={BTCImg} 
+        alt="BTC Image" 
+        className="cursor-pointer h-52 hover:h-60 w-52 hover:w-64 ease-in-out duration-300" 
+      />
     </div>
   );
 };
